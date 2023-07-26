d759f549a82bf9ecfa2b1f8533f011f86a79f6be
czh <1670343370@qq.com>
2023-07-26 15:22:37 +0800

    改变主题样式”

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+import ElementPlus from 'unplugin-element-plus/vite'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,12 +19,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle:"sass"})],
+    }),
+    ElementPlus({
+      useSource: true,
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+        @use "@/styles/element/index.scss" as*;
+        `
+
+      }
     }
   }
 })
